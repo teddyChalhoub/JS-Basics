6562f2ba1f928aa6231809219c7c5125b982e7aa
teddyChalhoub <teddychalhoub0@gmail.com>
2021-05-28 23:27:46 +0300

    Completed Step 6 : External files and Simple Math

---
+++ javascript-basics-01/js/Step_6.js
@@ -0,0 +1,6 @@
+var firstNumber = document.getElementById("first_number");
+var secondNumber = document.getElementById("second_number");
+
+document.getElementById("validate").addEventListener("click",function(){
+    alert(firstNumber.value % secondNumber.value);
+});
