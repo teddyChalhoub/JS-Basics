cb5f47e043378f25045f963ff776fc4c93ba2d96
teddyChalhoub <teddychalhoub0@gmail.com>
2021-05-28 22:44:55 +0300

    Step 2 : Variables 2

--- javascript-basics-01/js/Step_2.js
+++ javascript-basics-01/js/Step_2.js
@@ -2,4 +2,4 @@ var nom = "Doe";
 var prenom = "John";
 var ville = "New-York";
 
-alert("Nom : " + nom + "\n Prenon : " + prenom + "\n Ville : " + ville);
+alert("Nom : " + nom + "\n Prenom : " + prenom + "\n Ville : " + ville);
